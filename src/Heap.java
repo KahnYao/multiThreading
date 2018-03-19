a13e84a0ec5cf9f9e987bc5748e7ff2e12595762
KahnYao <1835755479@qq.com>
2018-03-19 10:46:32 +0800

    Updated

---
+++ src/Heap.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Heap {
+
+    public static void main(String[] args) {
+
+        ArrayList list = new ArrayList();
+        while (true)
+        {
+            list.add(new Heap());
+        }
+    }
+
+}
---
+++ src/Test30.java
@@ -0,0 +1,9 @@
+public class Test30 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            Student student = new Student();
+        }
+    }
+
+}
